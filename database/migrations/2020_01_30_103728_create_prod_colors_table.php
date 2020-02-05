0f29416fa9ae6268a66d4d427f23f3c0030dac86
Wagner G. de Castro <wagner2014.castro@gmail.com>
2020-02-05 18:02:01 -0300

    #09 - Correção na migration product e function removida anteriormente
    - Adicionada function formatNumToMysql removida por engano anteriormente
    - Corrigindo Controller Product devido alteração na estrutura da tabela product
    - Alterada chave estrangeira

--- database/migrations/2020_01_30_103728_create_prod_colors_table.php
+++ database/migrations/2020_01_30_103728_create_prod_colors_table.php
@@ -19,7 +19,7 @@ class CreateProdColorsTable extends Migration
             $table->foreign('id_product')->references('id')->on('products')->onDelete('cascade');
             $table->string('color_hexa', 9)->comment('code hexa, example: #00000000');
              $table->string('color_name', 60)->comment('description name color, example: black');
-            $table->index(['id_products','color_name']);
+            $table->index(['id_product','color_name']);
             //$table->primary(['color_hexa']);
             $table->timestamps();
         });
